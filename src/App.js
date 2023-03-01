fae5da0b12ab403aae7b448be466c2b59a01fd7b
luccamicale <luccamicale2015@gmail.com>
2023-03-01 12:06:07 -0300

    consume the api with react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import React from 'react';
+import Greeting from './pages/Greetings';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Marcos</h1>
-    </div>
+    <Greeting />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
